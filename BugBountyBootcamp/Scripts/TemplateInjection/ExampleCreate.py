fc6b695817c200e37baad5deeaf532c9a94fec14
Adam Markin <adam.markin@gmail.com>
2024-07-16 14:38:34 -0600

    ExampleCreate.py formatting fix

--- BugBountyBootcamp/Scripts/TemplateInjection/ExampleCreate.py
+++ BugBountyBootcamp/Scripts/TemplateInjection/ExampleCreate.py
@@ -1,8 +1,16 @@
 from jinja2 import Template
-    with open('example.jinja') as f:
-        tmpl = Template(f.read())
-    print(tmpl.render(
-        list_title = "Chapter Contents",
-        list_description = "Here are the contents of chapter 16.",
-        item_list = ["Mechanisms Of Template Injection", "Preventing Template Injection", "Hunting For Template Injection", "Escalating Template Injection", "Automating Template Injection", "Find your First Template Injection!"]
+
+with open('example.jinja') as f:
+    tmpl = Template(f.read())
+
+print(tmpl.render(
+    list_title="Chapter Contents",
+    list_description="Here are the contents of chapter 16.",
+    item_list=[
+        "Mechanisms Of Template Injection",
+        "Preventing Template Injection",
+        "Hunting For Template Injection",
+        "Escalating Template Injection",
+        "Automating Template Injection",
+        "Find your First Template Injection!"]
 ))
